4bfc16a5645208f3906c963af89d5b77f9d5b9ab
Viktor Forsberg <viktor.forsberg@skola.botkyrka.se>
2020-11-23 14:02:31 +0100

    button

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("Välkommen").addEventListener("click", knapp)
+
+function knapp () {
+ document.getElementById("Välkommsmeddelande").innerHTML = "ананасовые ебаные свиноеды"
+}
+
